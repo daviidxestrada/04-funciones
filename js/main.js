25e81d6696be937a8469aca5c11c899e729942f6
unknown <daviidxestrada@gmail.com>
2025-09-08 19:11:27 +0200

    Funciones explicacion

---
+++ js/main.js
@@ -0,0 +1,104 @@
+/*
+FUNCIONES DECLARADAS
+
+function nombreFunción(arg1,arg2){
+   bloque de instrucciones
+}
+
+nombreFuncion(arg1,arg2)
+
+*/
+
+//          let num1= undefined, let num2=undefined
+/*
+function sumar(num1, num2) {
+    let total= num1 + num2
+    // console.log(total)
+
+    return total
+
+}
+
+console.log(sumar(2, 3))
+console.log(sumar(30,20))
+console.log(sumar(5,5))
+*/
+
+/* funciones expresadas 
+
+const nombreVariable function(arg1,arg2){
+   bloque de instrucciones
+}
+
+nombreVariable()
+
+*/
+/*
+const multiplicar=function(num1,num2){
+    return num1 * num2
+}
+
+console.log(multiplicar(3,5))
+console.log(multiplicar(3,50))
+*/
+
+/*
+
+Funciones flecha
+
+const nombreVariable=(arg1,arg2)=>{
+    bloque de instrucciones
+    }
+
+*/
+
+
+/*
+const multiplicar=function(num1,num2){
+    return num1 * num2
+}
+
+const multiplicar2 = (num1,num2) =>  num1 * num2
+
+const mostrarMensaje = mensaje => mensaje
+
+const mostrarMensaje2 = function (mensaje){
+    return mensaje
+}
+
+
+console.log(multiplicar(3,5))
+console.log(multiplicar2(3,50))
+console.log(mostrarMensaje("Hola Mundo"))
+console.log(mostrarMensaje2("Hola Mundo desde mostrarMensaje2"))
+*/
+
+
+/*
+
+FUNCIONES CALLBACK
+
+*/
+/*
+function calcular(num1, num2, cb){
+
+
+     cb(num1, num2)
+     console.log(cb(num1, num2))
+
+}
+
+calcular(2,3,(num1, num2) => num1*num2)
+
+calcular(2,3,(num1, num2) =>num1 / num2)
+
+calcular(2,3,(num1, num2) => 2 * (num1 / num2))
+*/
+/*
+let contador = 0
+
+const incrementarContador=()=>{
+    contador += 1
+    return contador
+}
+*/
